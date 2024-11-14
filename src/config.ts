28acdb90479c73f94b0391046933881c1e792c12
Jose K James <josekjames5@gmail.com>
2024-11-14 23:02:17 +0530

    merge error fix

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   author: "Jose K James",
   profile: "",
-  desc: "A minimal, responsiv blog.",
+  desc: "Personal Blog of Jose",
   title: "Jose K James",
   ogImage: "me-og.jpg",
   lightAndDarkMode: true,
